389a209fd7ec95d865911eb7639101829527e6b8
Amy Pond <amypond@MacBook-Air-Amy.local>
2023-10-02 16:20:21 +0200

    updated Info component with using mapping props instead of rendering one-by-one manually

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -7,58 +7,74 @@ type FCProps = {
   data: Pick<Data, "id" | "height" | "weight" | "abilities" | "types">;
 };
 
+type MappedProps = {
+  plain: [
+    title: string,
+    key: keyof Omit<FCProps["data"], "abilities" | "types">,
+    value: number
+  ][];
+  list: [title: string, key: string, value: any][];
+};
+
+const mapProps = ({
+  id,
+  height,
+  weight,
+  types,
+  abilities,
+}: FCProps["data"]): MappedProps => {
+  return {
+    plain: [
+      ["ID", "id", id],
+      ["Height", "height", height],
+      ["Weight", "weight", weight],
+    ],
+    list: [
+      ["Types", "type", types],
+      ["Abilities", "ability", abilities],
+    ],
+  };
+};
+
 const renderTagsList = (tags: string[]): ReactNode[] => {
   return tags.map((tag) => (
     <Tag key={tag}>{TextTransform(tag, TextTransformations.upperCase)}</Tag>
   ));
 };
 
+const renderTitle = (title: string) => (
+  <span className="flex w-48 justify-end mr-2 text-lg text-gray-600">
+    {title}:
+  </span>
+);
+
 const Info: React.FC<FCProps> = ({ data }) => {
+  const { plain, list } = mapProps(data);
+
   return (
     <>
       <div className="flex flex-col justify-center w-full">
-        <div className="flex p-2">
-          <span className="flex w-48 justify-end mr-2 text-lg text-gray-600">
-            ID:
-          </span>
-          <span className="flex grow gap-4 justify-start text-lg text-gray-700">
-            #{data.id}
-          </span>
-        </div>
-        <div className="flex p-2">
-          <span className="flex w-48 justify-end mr-2 text-lg text-gray-600">
-            Height:
-          </span>
-          <span className="flex grow gap-4 justify-start text-lg text-gray-700">
-            {data.height * 10} cm
-          </span>
-        </div>
-        <div className="flex p-2">
-          <span className="flex w-48 justify-end mr-2 text-lg text-gray-600">
-            Weight:
-          </span>
-          <span className="flex grow gap-4 justify-start text-lg text-gray-700">
-            {data.weight / 10} kg
-          </span>
-        </div>
-        <div className="flex p-2">
-          <span className="flex w-48 justify-end mr-2 text-lg text-gray-600">
-            Abilities:
-          </span>
-          <span className="flex grow gap-4 justify-start text-lg text-gray-700">
-            {renderTagsList(data.types.map((types) => types.type.name))}
-          </span>
-        </div>
-        <div className="flex p-2">
-          <span className="flex w-48 justify-end mr-2 text-lg text-gray-600">
-            Type:
-          </span>
-          <span className="flex grow gap-4 justify-start text-lg text-gray-700">
-            {renderTagsList(
-              data.abilities.map((abilities) => abilities.ability.name)
-            )}
-          </span>
-        </div>
+        {plain.map(([title, key, value]) => (
+          <div className="flex p-2" key={key}>
+            {renderTitle(title)}
+            <span className="flex grow gap-4 justify-start text-lg text-gray-700">
+              {key === "id" && <span>#{value}</span>}
+              {key === "height" && <span>{value * 10} cm</span>}
+              {key === "weight" && <span>{data.weight / 10} kg</span>}
+            </span>
+          </div>
+        ))}
+
+        {list.map(([title, key, value]) => {
+          return (
+            <div className="flex p-2" key={key}>
+              {renderTitle(title)}
+              <span className="flex grow gap-4 justify-start text-lg text-gray-700">
+                {renderTagsList(value.map((v: any) => v[key].name))}
+              </span>
+            </div>
+          );
+        })}
       </div>
     </>
   );
